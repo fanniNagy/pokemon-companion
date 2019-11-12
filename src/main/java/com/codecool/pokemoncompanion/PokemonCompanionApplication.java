05c1c7b5d2dbf425c9b858f21aa8ae7d7108b9e7
Alex <alexalpek@gmail.com>
2019-11-12 12:54:42 +0100

    initial commit

---
+++ src/main/java/com/codecool/pokemoncompanion/PokemonCompanionApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.pokemoncompanion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonCompanionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokemonCompanionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codecool/pokemoncompanion/PokemonCompanionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.pokemoncompanion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonCompanionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

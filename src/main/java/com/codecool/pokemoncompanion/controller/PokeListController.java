a7c512fddccda1a4b46c09c2711e8760d78d42ec
Fanni <nagy.fanni.5@gmail.com>
2019-11-13 17:39:51 +0100

    distinguish routing by name or id

--- src/main/java/com/codecool/pokemoncompanion/controller/PokeListController.java
+++ src/main/java/com/codecool/pokemoncompanion/controller/PokeListController.java
@@ -6,13 +6,11 @@ import com.codecool.pokemoncompanion.model.generated.ResultsItem;
 import com.codecool.pokemoncompanion.service.PokeAPIService;
 import com.codecool.pokemoncompanion.service.PokemonCreator;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RequestMapping("/pokemon")
 @RestController
 public class PokeListController {
 
@@ -24,7 +22,7 @@ public class PokeListController {
 
 
     @CrossOrigin
-    @GetMapping("/pokes")
+    @GetMapping("/")
     public List<ResultsItem> pokes() {
         return pokeAPIService.getPokemons(20, 1);
     }
@@ -34,13 +32,13 @@ public class PokeListController {
 //        return pokeAPIService.getPokemonByID(id);
 //    }
 
-    @GetMapping("/pokemon/{id}")
+    @GetMapping("/id/{id}")
     public MyPokemon pokemonByID(@PathVariable("id") int id) {
         Pokemon pokemon =  pokeAPIService.getPokemonByID(id);
         return pokemonCreator.createPokemon(pokemon);
     }
 
-    @GetMapping("/pokemon/{name}")
+    @GetMapping("/name/{name}")
     public MyPokemon pokemonByName(@PathVariable("name") String name) {
         Pokemon pokemon =  pokeAPIService.getPokemonByName(name);
         return pokemonCreator.createPokemon(pokemon);

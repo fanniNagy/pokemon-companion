c940856223e02e8eb6752221bdeea2d52f0a72c7
Verhóczki Márton <verhoczki.marci@gmail.com>
2019-12-10 11:06:28 +0100

    Add CORS allowance for react

--- src/main/java/com/codecool/pokemoncompanion/controller/AuthController.java
+++ src/main/java/com/codecool/pokemoncompanion/controller/AuthController.java
@@ -10,16 +10,14 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/auth")
 @AllArgsConstructor

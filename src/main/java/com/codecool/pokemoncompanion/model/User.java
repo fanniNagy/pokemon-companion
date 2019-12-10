fb5ddfb6ef64b6606cf4c3e6f55eb516cd129084
Verhóczki Márton <verhoczki.marci@gmail.com>
2019-12-10 11:05:57 +0100

    Make user roles an elementCollection

--- src/main/java/com/codecool/pokemoncompanion/model/User.java
+++ src/main/java/com/codecool/pokemoncompanion/model/User.java
@@ -5,10 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
@@ -30,6 +27,7 @@ public class User {
     private String email;
     @NotNull
     private String password;
+    @ElementCollection(fetch = FetchType.EAGER)
     private List<String> roles = new ArrayList<>();
 
     @Override

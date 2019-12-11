a4afb9d193e54a323c0f7404c79069158de005c5
Verhóczki Márton <verhoczki.marci@gmail.com>
2019-12-11 15:11:18 +0100

    Add banned field to User

--- src/main/java/com/codecool/pokemoncompanion/DataInitializer.java
+++ src/main/java/com/codecool/pokemoncompanion/DataInitializer.java
@@ -23,25 +23,25 @@ public class DataInitializer implements CommandLineRunner {
     public void run(String... args) throws Exception {
         User user = User.builder()
                 .email("user@cc.com")
-                .id(1L)
                 .name("user")
                 .password(PasswordEncoderFactories.createDelegatingPasswordEncoder().encode("user"))
                 .roles(Collections.singletonList("ROLE_USER"))
                 .myPokemonsList(new ArrayList<>())
                 .favouritePokemonsList(new ArrayList<>())
                 .myPokemonWishList(new ArrayList<>())
+                .banned(true)
                 .build();
         userRepository.save(user);
 
         User admin = User.builder()
                 .email("admin@cc.com")
-                .id(1L)
                 .name("admin")
                 .password(PasswordEncoderFactories.createDelegatingPasswordEncoder().encode("admin"))
                 .roles(Collections.singletonList("ROLE_ADMIN"))
                 .myPokemonsList(new ArrayList<>())
                 .favouritePokemonsList(new ArrayList<>())
                 .myPokemonWishList(new ArrayList<>())
+                .banned(false)
                 .build();
         userRepository.save(admin);
     }

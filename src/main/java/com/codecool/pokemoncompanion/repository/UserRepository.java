f461cf115c46cfb468b600ca9dc0e4cb757240d1
fanniNagy <nagy.fanni.5@gmail.com>
2019-12-12 11:14:36 +0100

    add friend requesting and accepting services

--- src/main/java/com/codecool/pokemoncompanion/repository/UserRepository.java
+++ src/main/java/com/codecool/pokemoncompanion/repository/UserRepository.java
@@ -3,9 +3,11 @@ package com.codecool.pokemoncompanion.repository;
 import com.codecool.pokemoncompanion.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository <User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     User findFirstByOrderByEmailAsc();
 
     User findByName(String name);
+
+    User findUserById(Long id);
 }

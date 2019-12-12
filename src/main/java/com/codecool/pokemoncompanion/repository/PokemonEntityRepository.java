d04f839d737b0adfc5bfd7d1f4324b8505f5560d
Verhóczki Márton <verhoczki.marci@gmail.com>
2019-12-12 16:33:29 +0100

    Refactor repository layer for merge queries

--- src/main/java/com/codecool/pokemoncompanion/repository/PokemonEntityRepository.java
+++ src/main/java/com/codecool/pokemoncompanion/repository/PokemonEntityRepository.java
@@ -3,5 +3,5 @@ package com.codecool.pokemoncompanion.repository;
 import com.codecool.pokemoncompanion.model.PokemonEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MyFavouriteRepository extends JpaRepository<PokemonEntity, Long> {
+public interface PokemonEntityRepository extends JpaRepository<PokemonEntity, Long> {
 }

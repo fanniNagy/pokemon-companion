52b3dee19f66ab0e96d320ca6b4fc11f833bd939
fanniNagy <nagy.fanni.5@gmail.com>
2019-12-12 11:13:04 +0100

    optimize imports

--- src/main/java/com/codecool/pokemoncompanion/model/User.java
+++ src/main/java/com/codecool/pokemoncompanion/model/User.java
@@ -7,9 +7,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 @Data
 @AllArgsConstructor

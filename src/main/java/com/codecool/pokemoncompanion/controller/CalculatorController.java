49e3237f44b537d2dd3fd1371696d1b1e3255c73
fanniNagy <nagy.fanni.5@gmail.com>
2021-01-12 23:16:41 +0100

    remove redundant local variable

--- src/main/java/com/codecool/pokemoncompanion/controller/CalculatorController.java
+++ src/main/java/com/codecool/pokemoncompanion/controller/CalculatorController.java
@@ -3,13 +3,11 @@ package com.codecool.pokemoncompanion.controller;
 import com.codecool.pokemoncompanion.model.IVValues;
 import com.codecool.pokemoncompanion.service.CalculatorService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
 @RestController
+@RequestMapping("/pokemon")
 public class CalculatorController {
 
     @Autowired
@@ -18,8 +16,7 @@ public class CalculatorController {
 
     @PostMapping("/ivcalculator")
     public String sendIvValue(@RequestBody IVValues ivValues) {
-        String result = calculatorService.calculateIV(ivValues.getHp(), ivValues.getAttack(), ivValues.getDefense());
-        return result;
+        return calculatorService.calculateIV(ivValues.getHp(), ivValues.getAttack(), ivValues.getDefense());
     }
 
 }
